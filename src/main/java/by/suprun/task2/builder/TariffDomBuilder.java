154374fc7cb205fb569e4edcb9f83587339943a7
Philip Suprun <filipplihacev95@gmail.com>
2022-03-14 18:28:28 +0300

    17.55 14.03.2022

--- src/main/java/by/suprun/task2/builder/TariffDomBuilder.java
+++ src/main/java/by/suprun/task2/builder/TariffDomBuilder.java
@@ -4,9 +4,23 @@ import by.suprun.task2.exception.TariffException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 public class TariffDomBuilder extends AbstractTariffBuilder {
     private static final Logger logger = LogManager.getLogger();
+    private final DocumentBuilder documentBuilder;
 
+
+    public TariffDomBuilder()throws TariffException{
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        try{
+            documentBuilder = factory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            logger.error("Dom configuration encounter error", e);
+        }
+    }
     @Override
     public void buildTariffs(String path) throws TariffException {
 
